d471529bbd84297c169f3aa9161ae423cd679fa5
feilong <614183595@qq.com>
2018-03-25 18:17:56 +0800

    add tips

--- pages/cimoc/_id.vue
+++ pages/cimoc/_id.vue
@@ -1,15 +1,21 @@
 <template>
   <section>
-    <h1>
+    <b-alert show>
       {{id}}
-    </h1>
+    </b-alert>
     <fl-disqus :pageId="id" />
+    <b-media>
+      <h5 class="mt-0">Tips</h5>
+      <p class="mb-0">
+        本网站基于 Disqus 服务，仅能在部分国家或地区使用。如果您无法加载讨论，请检查您当地的网络服务政策。
+      </p>
+    </b-media>
   </section>
 </template>
 
 <script>
   import disqus from '~/components/disqus/disqus.vue'
-  import Vue from 'vue'
+  // import Vue from 'vue'
 
   export default {
     components: {
